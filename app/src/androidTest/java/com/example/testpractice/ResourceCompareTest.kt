170c49e313484d54002ee293b20e418d88e5b046
lubshad <lubshadprimalcodes@gmail.com>
2021-10-23 17:10:56 +0530

    Befor and After functions practiced with app context

--- app/src/androidTest/java/com/example/testpractice/ResourceCompareTest.kt
+++ app/src/androidTest/java/com/example/testpractice/ResourceCompareTest.kt
@@ -1,18 +1,38 @@
 package com.example.testpractice
 
 import android.content.Context
+import android.util.Log
 import androidx.test.core.app.ApplicationProvider
 import com.google.common.truth.Truth.assertThat
+import org.junit.After
+import org.junit.Before
 import org.junit.Test
 
 class ResourceCompareTest {
 
-    val resourceCompare = ResourceCompare()
+    private  lateinit var resourceCompare: ResourceCompare
+
+    @Before
+    fun setup() {
+        resourceCompare = ResourceCompare()
+    }
+
+    @After
+    fun tearDown() {
+        Log.i("TearDown", "Cleared")
+    }
 
     @Test
     fun inputAppNameReturnsTrue() {
        val context = ApplicationProvider.getApplicationContext<Context>()
        val result = resourceCompare.isEqual(context = context, R.string.app_name, "Test Practice")
         assertThat(result).isTrue()
+    }
+
+    @Test
+    fun inputRandomNameReturnsFalse() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        val result = resourceCompare.isEqual(context = context, R.string.app_name, "Random Name")
+        assertThat(result).isFalse()
     }
 }
